cf2d7d3bed347b52e049c96fd05e68cffcdf611f
Kiany Sandoval <sandovalkiany@gmail.com>
2026-03-23 20:48:04 -0600

    Correción de V-08 y documentacion

--- sqli_lab_vulnerable_app/app.py
+++ sqli_lab_vulnerable_app/app.py
@@ -25,11 +25,13 @@ import sqlite3
 import os
 from pathlib import Path
 from werkzeug.security import generate_password_hash
+from flask_wtf.csrf import CSRFProtect
 
 BASE_DIR = Path(__file__).resolve().parent
 DB_PATH  = BASE_DIR / "db" / "lab.db"
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 # ---------------------------------------------------------------
 # V-04: SECRET_KEY hardcodeada en el código fuente.
